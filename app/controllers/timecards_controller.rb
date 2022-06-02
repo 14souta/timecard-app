e764ea5710d57f546a4b9e7b939a39a09ae805f4
14souta <tagusota@gmail.com>
2022-06-02 12:16:39 +0900

    first commit

---
+++ app/controllers/timecards_controller.rb
@@ -0,0 +1,9 @@
+class TimecardsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root to: "timecards#index"
+end
