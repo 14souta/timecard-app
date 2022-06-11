de3143c30a3c3bdceee66e31d3553682bcfee0f5
14souta <tagusota@gmail.com>
2022-06-11 12:17:28 +0900

    HTMLの調整

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: "timecards#index"
   resources :users, only: [:edit, :update]
   resources :timecards, only: [:index, :new]
+  resources :starts,  only: [:new]
+  resources :stops, only: [:new]
+
 end
