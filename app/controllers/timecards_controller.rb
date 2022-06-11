822d959ce237a57615a863676484780519f8ca93
14souta <tagusota@gmail.com>
2022-06-11 11:26:23 +0900

    htmlの修正

--- app/controllers/timecards_controller.rb
+++ app/controllers/timecards_controller.rb
@@ -1,11 +1,28 @@
 class TimecardsController < ApplicationController
 
   def index
+    @timecard = Timecard.all
   end
 
   def new
+    @timecard = Timecard.new
   end
   
-  
+  def create
+    @timecard = Timecard.new(time_params)
+    if @timecard.save
+       redirect_to root_path
+    elsif @timecard.stop_time == nil
+      render :new
+    else
+       redirect_to root_path
+   end
+  end
 
+
+  private
+
+  def time_params
+    params.permit(:name, :start_time, :stop_time, :day)
+  end
 end
--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -1,5 +1,4 @@
 class Timecard < ApplicationRecord
-  has_one :start_time
-  has_one :stop_time
   belongs_to :user
+
 end
